5574ecaa4a07743b7a62bc581ace359f5ad8d4cf
fogelito <fogelshmuel@gmail.com>
2024-07-09 17:25:49 +0300

    Datetime resource

--- src/Migration/Resources/Database/Attributes/DateTime.php
+++ src/Migration/Resources/Database/Attributes/DateTime.php
@@ -7,47 +7,29 @@ use Utopia\Migration\Resources\Database\Collection;
 
 class DateTime extends Attribute
 {
-    public function __construct(
-        string $key,
-        Collection $collection,
-        bool $required = false,
-        bool $array = false,
-        private readonly ?string $default = null,
-    ) {
+    protected ?string $default;
+
+    /**
+     * @param  ?string  $default
+     */
+    public function __construct(string $key, Collection $collection, bool $required = false, bool $array = false, string $default = null)
+    {
         parent::__construct($key, $collection, $required, $array);
-    }
-
-    /**
-     * @param  array<string, mixed>  $array
-     */
-    public static function fromArray(array $array): self
-    {
-        return new self(
-            $array['key'],
-            Collection::fromArray($array['collection']),
-            $array['required'] ?? false,
-            $array['array'] ?? false,
-            $array['default'] ?? null
-        );
-    }
-
-    /**
-     * @return array<string, mixed>
-     */
-    public function jsonSerialize(): array
-    {
-        return array_merge(parent::jsonSerialize(), [
-            'default' => $this->default,
-        ]);
-    }
-
-    public function getTypeName(): string
-    {
-        return Attribute::TYPE_DATETIME;
+        $this->default = $default;
     }
 
     public function getDefault(): ?string
     {
         return $this->default;
+    }
+
+    public function setDefault(string $default): void
+    {
+        $this->default = $default;
+    }
+
+    public function getTypeName(): string
+    {
+        return Attribute::TYPE_DATETIME;
     }
 }
