d63024ce593124a5ec47f9606386bc01ba27424c
fogelito <fogelshmuel@gmail.com>
2024-07-21 15:03:39 +0300

    Fix no length

--- src/Migration/Sources/Appwrite.php
+++ src/Migration/Sources/Appwrite.php
@@ -1035,6 +1035,7 @@ class Appwrite extends Source
                         $collection,
                         $index['type'],
                         $index['attributes'],
+                        [],
                         $index['orders']
                     );
                 }
