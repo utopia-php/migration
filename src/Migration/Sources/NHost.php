1a3eaa563572a07ac8b799f1b4cb5e8bbfe08bad
Jake Barnby <jakeb994@gmail.com>
2024-07-25 14:49:42 +1200

    Fix array check

--- src/Migration/Sources/NHost.php
+++ src/Migration/Sources/NHost.php
@@ -487,7 +487,7 @@ class NHost extends Source
                         $processedData = [];
                         foreach ($collectionAttributes as $attribute) {
                             /** @var Attribute $attribute */
-                            if (! $attribute->getArray() && \is_array($data[$attribute->getKey()])) {
+                            if (! $attribute->isArray() && \is_array($data[$attribute->getKey()])) {
                                 $processedData[$attribute->getKey()] = json_encode($data[$attribute->getKey()]);
                             } else {
                                 $processedData[$attribute->getKey()] = $data[$attribute->getKey()];
