110cc870cd4274a13cac0d5bec890cb5539ba305
Jake Barnby <jakeb994@gmail.com>
2025-03-18 16:14:56 +1300

    Interface doc

--- src/Migration/Sources/Appwrite/Reader.php
+++ src/Migration/Sources/Appwrite/Reader.php
@@ -11,6 +11,13 @@ use Utopia\Migration\Resources\Database\Database;
  */
 interface Reader
 {
+    /**
+     * Get information about the resources currently on the source
+     *
+     * @param array $resources
+     * @param array $report
+     * @return mixed
+     */
     public function report(array $resources, array &$report);
 
     /**
@@ -22,6 +29,8 @@ interface Reader
     public function listDatabases(array $queries = []): array;
 
     /**
+     * List collections that match the given queries
+     *
      * @param Database $resource
      * @param array $queries
      * @return array
@@ -29,6 +38,8 @@ interface Reader
     public function listCollections(Database $resource, array $queries = []): array;
 
     /**
+     * List attributes that match the given queries
+     *
      * @param Collection $resource
      * @param array<QueryType> $queries
      * @return array
@@ -36,6 +47,8 @@ interface Reader
     public function listAttributes(Collection $resource, array $queries = []): array;
 
     /**
+     * List indexes that match the given queries
+     *
      * @param Collection $resource
      * @param array<QueryType> $queries
      * @return array
@@ -43,6 +56,8 @@ interface Reader
     public function listIndexes(Collection $resource, array $queries = []): array;
 
     /**
+     * List documents that match the given queries
+     *
      * @param Collection $resource
      * @param array<QueryType> $queries
      * @return array
@@ -50,6 +65,8 @@ interface Reader
     public function listDocuments(Collection $resource, array $queries = []): array;
 
     /**
+     * Get a document by its ID in the given collection
+     *
      * @param Collection $resource
      * @param string $documentId
      * @param array $queries
@@ -58,12 +75,16 @@ interface Reader
     public function getDocument(Collection $resource, string $documentId, array $queries = []): array;
 
     /**
+     * Return a query to select the given attributes
+     *
      * @param array $attributes
      * @return QueryType|string
      */
     public function querySelect(array $attributes): mixed;
 
     /**
+     * Return a query to filter the given attributes
+     *
      * @param string $attribute
      * @param array $values
      * @return QueryType|string
@@ -71,12 +92,16 @@ interface Reader
     public function queryEqual(string $attribute, array $values): mixed;
 
     /**
+     * Return a query to paginate after the given resource
+     *
      * @param Resource|string $resource
      * @return QueryType|string
      */
     public function queryCursorAfter(Resource|string $resource): mixed;
 
     /**
+     * Return a query to limit the number of results
+     * 
      * @param int $limit
      * @return QueryType|string
      */
