b308d9183f1f8ab32e172f31744ad93db319cba9
Darshan <itznotabug@gmail.com>
2025-04-09 18:24:03 +0530

    update: pagination and attribute type handling.

--- src/Migration/Sources/Csv.php
+++ src/Migration/Sources/Csv.php
@@ -2,32 +2,43 @@
 
 namespace Utopia\Migration\Sources;
 
+use Utopia\Database\Database as UtopiaDatabase;
 use Utopia\Migration\Exception;
 use Utopia\Migration\Resource;
+use Utopia\Migration\Resources\Database\Attribute;
 use Utopia\Migration\Resources\Database\Collection;
 use Utopia\Migration\Resources\Database\Database;
 use Utopia\Migration\Resources\Database\Document;
 use Utopia\Migration\Resources\Storage\File;
 use Utopia\Migration\Source;
+use Utopia\Migration\Sources\Appwrite\Reader;
+use Utopia\Migration\Sources\Appwrite\Reader\Database as DatabaseReader;
 use Utopia\Migration\Transfer;
 use Utopia\Storage\Device;
 
 class Csv extends Source
 {
-    public string $filePath;
+    private string $filePath;
 
     /**
      * format: `{databaseId:collectionId}`
      */
-    public string $resourceId;
+    private string $resourceId;
 
-    public Device $deviceForCsvImports;
+    private Device $deviceForCsvImports;
 
-    public function __construct(string $resourceId, string $filePath, Device $deviceForCsvImports)
-    {
+    private Reader $database;
+
+    public function __construct(
+        string $resourceId,
+        string $filePath,
+        Device $deviceForCsvImports,
+        ?UtopiaDatabase $dbForProject
+    ) {
         $this->filePath = $filePath;
         $this->resourceId = $resourceId;
         $this->deviceForCsvImports = $deviceForCsvImports;
+        $this->database = new DatabaseReader($dbForProject);
     }
 
     public static function getName(): string
@@ -74,6 +85,9 @@ class Csv extends Source
         }
     }
 
+    /**
+     * @throws Exception|\Utopia\Database\Exception
+     */
     private function exportDocuments(int $batchSize): void
     {
         if (! $this->deviceForCsvImports->exists($this->filePath)) {
@@ -91,9 +105,57 @@ class Csv extends Source
             return;
         }
 
+        $allAttributes = [];
+        $lastAttribute = null;
+
         [$databaseId, $collectionId] = explode(':', $this->resourceId);
         // TODO: @itznotabug, @jake - do we need to check for permissions here or db handles it?
         $collection = new Collection(new Database($databaseId, ''), '', $collectionId);
+
+        while (true) {
+            // paginate over the attributes
+            $queries = [$this->database->queryLimit($batchSize)];
+            if ($lastAttribute) {
+                $queries[] = $this->database->queryCursorAfter($lastAttribute);
+            }
+
+            $fetched = $this->database->listAttributes($collection, $queries);
+            if (empty($fetched)) {
+                break;
+            }
+
+            $allAttributes = array_merge($allAttributes, $fetched);
+            $lastAttribute = $fetched[count($fetched) - 1];
+
+            if (count($fetched) < $batchSize) {
+                break;
+            }
+        }
+
+        $attributeTypes = [];
+        $manyToManyKeys = [];
+
+        foreach ($allAttributes as $attribute) {
+            $key = $attribute['key'];
+
+            if (
+                // Skip child-side relationships entirely
+                $attribute['type'] === Attribute::TYPE_RELATIONSHIP &&
+                ($attribute['side'] ?? '') === UtopiaDatabase::RELATION_SIDE_CHILD
+            ) {
+                continue;
+            }
+
+            $attributeTypes[$key] = $attribute['type'];
+
+            if (
+                $attribute['type'] === Attribute::TYPE_RELATIONSHIP &&
+                ($attribute['relationType'] ?? '') === 'manyToMany' &&
+                ($attribute['side'] ?? '') === 'parent'
+            ) {
+                $manyToManyKeys[] = $key;
+            }
+        }
 
         $buffer = [];
 
@@ -103,8 +165,51 @@ class Csv extends Source
                 continue;
             }
 
-            $docId = $data['$id'] ?? 'unique()';
-            $document = new Document($docId, $collection, $data);
+            $parsedData = $data;
+
+            foreach ($data as $key => $value) {
+                if (! isset($attributeTypes[$key])) {
+                    continue;
+                }
+
+                $type = $attributeTypes[$key];
+                $parsedValue = trim($value);
+
+                if ($parsedValue === '') {
+                    $parsedData[$key] = null;
+
+                    continue;
+                }
+
+                // TODO: @itznotabug, @jake - should we support Relationships like these?
+                if (in_array($key, $manyToManyKeys, true)) {
+                    $parsedData[$key] = str_contains($parsedValue, ',')
+                        ? array_map('trim', explode(',', $parsedValue))
+                        : [$parsedValue];
+
+                    continue;
+                }
+
+                switch ($type) {
+                    case Attribute::TYPE_INTEGER:
+                        $parsedData[$key] = is_numeric($parsedValue) ? (int) $parsedValue : null;
+                        break;
+
+                    case Attribute::TYPE_FLOAT:
+                        $parsedData[$key] = is_numeric($parsedValue) ? (float) $parsedValue : null;
+                        break;
+
+                    case Attribute::TYPE_BOOLEAN:
+                        $parsedData[$key] = filter_var($parsedValue, FILTER_VALIDATE_BOOLEAN);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            $docId = $parsedData['$id'] ?? 'unique()';
+            $document = new Document($docId, $collection, $parsedData);
 
             $buffer[] = $document;
 
